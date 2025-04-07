7c2bacc5c03907fcf1d898c4be3104c018c18f84
Ranger md <153409985+Marksimiyu@users.noreply.github.com>
2025-04-07 14:45:49 +0300

    Create prince.js

---
+++ prince.js
@@ -0,0 +1,26 @@
+{
+  "bot-name": Destiny md",
+  "description": "ᴊᴀᴠᴀsᴄʀɪᴘᴛ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ MARKDEVS ❣️ᴛᴇᴄʜ",
+  "logo": "https://raw.githubusercontent.com/Mayelprince/url/main/url/1264.jpg",
+  "keywords": ["bot"],
+  "documentation-link": "https://www-devspace-com.onrender.com",
+  "owner-verification": "marksimiyu050@gmail.com",
+  
+  "env": {
+    "SESSION_ID": {
+      "description": "Put your SESSION_ID here. Make sure it starts with destiny md-",
+      "value": "",
+      "required": true
+    },
+    "PREFIX": {
+      "description": "Bot command prefix, (e.g, . , ! @ etc)",
+      "value": ".",
+      "required": true
+    },
+    "MODE": {
+      "description": "Bot mode can be public, private, inbox, or groups (in lowercase) (e.g., public)",
+      "value": "private",
+      "required": true
+    }
+  }
+}
